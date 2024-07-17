0cfccef542ff82482705acb0990c39da9a67aac1
Csar24 <gr14079@ues.edu.sv>
2024-07-17 13:14:00 -0600

    1.1 Consumo de Api

--- src/main/java/com/csar/api/ApiApplication.java
+++ src/main/java/com/csar/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package com.csar.api;
 
+import com.csar.api.services.ConsumoAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,13 @@ public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+		ConsumoAPI consumoAPI =new ConsumoAPI();
+		String json = consumoAPI.obtnerDatos("https://www.omdbapi.com/?t=matrix&apikey=ae5358c3");
+		System.out.println(json );
+
 	}
+
+
+
 
 }
